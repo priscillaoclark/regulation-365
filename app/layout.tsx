6cdfb166318c4251d8710966c319bdf1537d5625
Priscilla Clark <17732979+priscillaoclark@users.noreply.github.com>
2024-10-08 22:38:01 -0400

    add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { EnvVarWarning } from "@/components/env-var-warning";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import HeaderAuth from "@/components/header-auth";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { GeistSans } from "geist/font/sans";
@@ -43,6 +44,7 @@ export default function RootLayout({
               </nav>
               <div className="flex flex-col gap-20 max-w-5xl p-5">
                 {children}
+                <SpeedInsights />
               </div>
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
