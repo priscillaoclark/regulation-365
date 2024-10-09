7c2bdac48135c687a1d9c8fb1822fe2edb240f0c
Priscilla Clark <17732979+priscillaoclark@users.noreply.github.com>
2024-10-09 13:28:02 -0400

    Removed sign up

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -9,23 +9,26 @@ export default function Login({ searchParams }: { searchParams: Message }) {
   return (
     <form className="flex-1 flex flex-col min-w-64">
       <h1 className="text-2xl font-medium">Sign in</h1>
-      <p className="text-sm text-foreground">
+      <p className="text-xs text-foreground italic">
+        New sign ups coming soon!
+      </p>
+      {/* <p className="text-sm text-foreground">
         Don't have an account?{" "}
         <Link className="text-foreground font-medium underline" href="/sign-up">
           Sign up
         </Link>
-      </p>
+      </p> */}
       <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
         <Label htmlFor="email">Email</Label>
         <Input name="email" placeholder="you@example.com" required />
         <div className="flex justify-between items-center">
           <Label htmlFor="password">Password</Label>
-          <Link
+          {/* <Link
             className="text-xs text-foreground underline"
             href="/forgot-password"
-          >
+            >
             Forgot Password?
-          </Link>
+            </Link> */}
         </div>
         <Input
           type="password"
@@ -33,7 +36,11 @@ export default function Login({ searchParams }: { searchParams: Message }) {
           placeholder="Your password"
           required
         />
-        <SubmitButton pendingText="Signing In..." formAction={signInAction}>
+        <SubmitButton
+          className="bg-lime-400"
+          pendingText="Signing In..."
+          formAction={signInAction}
+        >
           Sign in
         </SubmitButton>
         <FormMessage message={searchParams} />
