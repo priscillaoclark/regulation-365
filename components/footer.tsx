a3049685c6156f96bb313158a036366b54a100fc
Priscilla Clark <17732979+priscillaoclark@users.noreply.github.com>
2024-10-09 14:48:21 -0400

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,12 +6,12 @@ const Footer = () => {
   return (
     <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-s">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
+        {/* <div className="sm:flex sm:items-center sm:justify-between">
           <div className="justify-left">
             <AppLogo />
           </div>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-            {/* <li>
+            <li>
               <a href="/about" className="hover:underline me-4 md:me-6">
                 About
               </a>
@@ -25,10 +25,10 @@ const Footer = () => {
               <a href="/contact" className="hover:underline">
                 Contact
               </a>
-            </li>*/}
+            </li>
           </ul>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />*/}
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           &copy; {new Date().getFullYear()} Regulation 365. All rights reserved.
         </span>
